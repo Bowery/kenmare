16dae5fc80fb8267fa51e0716ad3e7f843cb5409
Steve Kaliski <sjkaliski@gmail.com>
2014-09-02 11:48:11 -0400

    fetch dev via broome
    fix
    
    wtf

--- routes.go
+++ routes.go
@@ -10,6 +10,7 @@ import (
 
 	"code.google.com/p/go-uuid/uuid"
 
+	"github.com/Bowery/gopackages/config"
 	"github.com/Bowery/gopackages/requests"
 	"github.com/Bowery/gopackages/schemas"
 	"github.com/gorilla/mux"
@@ -57,9 +58,10 @@ func healthzHandler(rw http.ResponseWriter, req *http.Request) {
 	fmt.Fprintln(rw, "ok")
 }
 
-type createEnvironmentReq struct {
+type createApplicationReq struct {
 	AMI          string `json:"ami"`
 	EnvID        string `json:"envID"`
+	Token        string `json:"token"`
 	InstanceType string `json:"instance_type"`
 	AWSAccessKey string `json:"aws_access_key"`
 	AWSSecretKey string `json:"aws_secret_key"`
@@ -68,23 +70,27 @@ type createEnvironmentReq struct {
 
 // createEnvironmentHandler creates a new environment
 func createApplicationHandler(rw http.ResponseWriter, req *http.Request) {
-	var body createEnvironmentReq
+	var body createApplicationReq
 	decoder := json.NewDecoder(req.Body)
 	err := decoder.Decode(&body)
 	if err != nil {
 		r.JSON(rw, http.StatusBadRequest, map[string]string{
-			"error": err.Error(),
+			"status": requests.STATUS_FAILED,
+			"error":  err.Error(),
 		})
 		return
 	}
 
 	ami := body.AMI
+	token := body.Token
 	instanceType := body.InstanceType
 	awsAccessKey := body.AWSAccessKey
 	awsSecretKey := body.AWSSecretKey
 	ports := body.Ports
 
-	if ami == "" || instanceType == "" || awsAccessKey == "" || awsSecretKey == "" {
+	// Validate request.
+	if ami == "" || token == "" || instanceType == "" ||
+		awsAccessKey == "" || awsSecretKey == "" {
 		r.JSON(rw, http.StatusBadRequest, map[string]string{
 			"status": requests.STATUS_FAILED,
 			"error":  "missing fields",
@@ -92,6 +98,16 @@ func createApplicationHandler(rw http.ResponseWriter, req *http.Request) {
 		return
 	}
 
+	// Get developer via token from Broome.
+	dev, err := getDev(token)
+	if err != nil {
+		r.JSON(rw, http.StatusBadRequest, map[string]string{
+			"status": requests.STATUS_FAILED,
+			"error":  err.Error(),
+		})
+	}
+
+	// Create AWS client.
 	awsClient, err := NewAWSClient(awsAccessKey, awsSecretKey)
 	if err != nil {
 		r.JSON(rw, http.StatusBadRequest, map[string]string{
@@ -120,20 +136,22 @@ func createApplicationHandler(rw http.ResponseWriter, req *http.Request) {
 		}
 	}
 
-	// Create app
+	// Create app.
 	appID := uuid.New()
 	envID := uuid.New()
 
 	app := schemas.Application{
-		ID:     appID,
-		EnvID:  envID,
-		Status: "provisioning",
+		ID:          appID,
+		EnvID:       envID,
+		DeveloperID: dev.ID.Hex(),
+		Status:      "provisioning",
 	}
 
+	// Write to Orchestrate.
 	_, err = db.Put("applications", appID, app)
 	if err != nil {
 		r.JSON(rw, http.StatusBadRequest, map[string]string{
-			"status": requests.STATUS_SUCCESS,
+			"status": requests.STATUS_FAILED,
 			"error":  err.Error(),
 		})
 		return
@@ -305,4 +323,32 @@ func createEventHandler(rw http.ResponseWriter, req *http.Request) {
 		"status": requests.STATUS_SUCCESS,
 		"event":  event,
 	})
+}
+
+type developerRes struct {
+	Status    string             `json:"status"`
+	Err       string             `json:"error"`
+	Developer *schemas.Developer `json:"developer"`
+}
+
+func getDev(token string) (*schemas.Developer, error) {
+	addr := fmt.Sprintf("%s/developers/me?token=%s", config.BroomeAddr, token)
+	res, err := http.Get(addr)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	devRes := new(developerRes)
+	decoder := json.NewDecoder(res.Body)
+	err = decoder.Decode(devRes)
+	if err != nil {
+		return nil, err
+	}
+
+	if devRes.Status == requests.STATUS_FOUND {
+		return devRes.Developer, nil
+	}
+
+	return nil, err
 }
